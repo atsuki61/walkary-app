252195e85895d184312d59cd1370b79700a57e4e
E22C1034-nishida <keixitian36@gmail.com>
2025-01-24 15:05:24 +0900

    a

--- staticfiles/walkary/js/update_remaining.js
+++ staticfiles/walkary/js/update_remaining.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const remainingDistance = Math.max(goal - walkedDistance, 0);
 
     const remainingElement = document.getElementById("remaining");
-    remainingElement.innerText = `${remainingDistance.toFixed(2)} m`;
+    remainingElement.innerText = `${remainingDistance.toFixed(0)} m`;
     window.addEventListener("storage", (event) => {
         if (event.key === "walked_distance") {
             const newWalkedDistance = JSON.parse(event.newValue) || 0;
             const newRemainingDistance = Math.max(goal - newWalkedDistance, 0);
             // remainingElement.innerText = `${newRemainingDistance} m`;
-            remainingElement.innerText = `${newRemainingDistance.toFixed(2)} m`;
+            remainingElement.innerText = `${newRemainingDistance.toFixed(0)} m`;
         }
     });
 });
