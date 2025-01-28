450a5289b38cf686086fc62412ddbdd288f1b915
E22C1034-nishida <keixitian36@gmail.com>
2025-01-29 00:57:31 +0900

    touroku

--- walkary/forms.py
+++ walkary/forms.py
@@ -15,8 +15,11 @@ class UserCreateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'ユーザーID'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'パスワード'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = '確認用パスワード'
 
     class Meta:
         model = User
